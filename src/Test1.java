65d2c28e65972d97e7e51c9e0beb9b2f325074a7
sudha <sudha@DESKTOP-VVU47BJ>
2021-12-10 16:05:03 +0530

    First Commit...

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 
+{
+	public static void main(String[] args) {
+		
+		String Hungry="yes";
+		
+		if(Hungry == "yes")
+			System.out.println("eat somthing...........");
+		else {
+			System.out.println("Do work ........!!!");
+		}
+	}
+}
